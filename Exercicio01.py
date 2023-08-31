8462ff257b50a15b0bc9353c098e7d9dcf5847e1
JvAmbrozio98 <jvambrozio98@gmail.com>
2023-08-30 23:08:32 -0300

    Exercicio01 feito

---
+++ Exercicio01.py
@@ -0,0 +1,23 @@
+import random
+
+def imprimirNumerosAnterioresPosteriores(numero:int):
+    numerosAnteriores = []
+    numerosPosteriores = []
+
+    i = numero - 50
+    while i < numero:
+        numerosAnteriores.append(i)
+        i += 1
+       
+
+    i = numero + 1
+    while i < numero + 51:
+        numerosPosteriores.append(i)
+        i += 1
+
+    print(f'\n Os numeros anteriores sao:{numerosAnteriores} \n')
+    print(f'O numero digitado foi: {numero} \n')
+    print(f'Os numeros posteriores sao:{numerosPosteriores} \n')
+
+
+imprimirNumerosAnterioresPosteriores(random.randint(0,100000))
