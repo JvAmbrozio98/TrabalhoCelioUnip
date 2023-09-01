52c7b0e18f3070f9b5f3e118a6306c3b73f772c5
JvAmbrozio98 <jvambrozio98@gmail.com>
2023-09-01 09:42:53 -0300

    Feito agora

--- Exercicio05.py
+++ Exercicio05.py
@@ -1,7 +1,7 @@
 #
 import random
-from Exercicio5ConstrutoresEfuncoes import Municipio
-from Exercicio5ConstrutoresEfuncoes import distribuirPopulacao
+from Exercicio5ConstrutoresEfuncoes import Municipio,distribuirPopulacao,testeAutomatizado,entradaPeloUsuario,escolhaDeModo
+
 
 
 listaDeCidades = []
@@ -11,40 +11,26 @@ nomesCidadesReais = [
 ]
 x = 0
 
+instanciaDeCidade = escolhaDeModo()
+
+
+
 
 
 while(x < 10):
-    listaInputdoUsuario = []
-    nomeDigitadoPeloUsuario = input("Digita o nome da cidade:") 
     
-    votosEmBrancoUser = int(input("Quantos votos em branco na cidade: "))
-    listaInputdoUsuario.append(votosEmBrancoUser)
-
-    votosNulosUser = int(input("Quantos votos nulos na cidade: "))
-    listaInputdoUsuario.append(votosNulosUser)
     
-    votosValidosUser = int(input("Quantos votos validos na cidade: "))
-    listaInputdoUsuario.append(votosValidosUser)
+    
    
-
-    # numeroAleatorio = random.randint(1,10000000000000)
-    # populacaoAleatoria = distribuirPopulacao(numeroAleatorio,3)
-    # instanciaDeCidade = Municipio(nomesCidadesReais[x],populacaoAleatoria)
-    instanciaDeCidade = Municipio(nomeDigitadoPeloUsuario,listaInputdoUsuario)
-   
-    
-    
-    
-    
-    print(f'\n**********************************************************************')
-    print(f'\n Na cidade de : {instanciaDeCidade.nome}')
-    print(f'\nO numero de votos total  eh: {instanciaDeCidade.totalDeVotos}')
+    print(f'\n**********************************************************************')    
+    print(f'\n Na cidade de : {instanciaDeCidade.nome}')    
+    print(f'\nO numero de votos total  eh: {instanciaDeCidade.totalDeVotos}')    
     print(f'\nO numero de votos em branco eh: {instanciaDeCidade.votosEmBranco}')
     print(f'\nO numero de votos  nulos eh: {instanciaDeCidade.votosNulos}')
     print(f'\nO numero de votos validos eh: {instanciaDeCidade.votosValidos}')
-    print(f'\n A porcentagem de votos em Branco: {instanciaDeCidade.porcentagemDeVotosEmBranco:.2f} %')
-    print(f'\n A porcentagem de votos em Nulos: {instanciaDeCidade.porcentagemDeVotosNulos:.2f} %')
-    print(f'\n A porcentagem de votos em Validos: {instanciaDeCidade.porcentagemDeVotosValidos:.2f} %')
+    print(f'\n A porcentagem de votos brancos: {instanciaDeCidade.porcentagemDeVotosEmBranco:.2f} %')
+    print(f'\n A porcentagem de votos  nulos: {instanciaDeCidade.porcentagemDeVotosNulos:.2f} %')
+    print(f'\n A porcentagem de votos validos: {instanciaDeCidade.porcentagemDeVotosValidos:.2f} %')
     print(f'\n**********************************************************************')
     listaDeCidades.append(instanciaDeCidade)
     x += 1
@@ -70,16 +56,16 @@ print(f'\n**********************************************************************
 cidadeComMaisVotosNulos = max(listaDeCidades,key=lambda x: x.votosNulos)
 print(f'**********************************************************************')
 print(f'A cidade com mais votos Nulos eh : {cidadeComMaisVotosNulos.nome} \n ')
-print(f'A quantidade total de votos eh : {cidadeComMaisVotosNulos.votosNulos} \n ')
-print(f'Que equivale a :   {cidadeComMaisVotosValidos.porcentagemDeVotosNulos:.2f}  % \n ')
+print(f'A quantidade total de votos nulos eh : {cidadeComMaisVotosNulos.votosNulos} \n ')
+print(f'Que equivale a :   {cidadeComMaisVotosNulos.porcentagemDeVotosNulos:.2f}  % \n ')
 print(f'\n**********************************************************************')
 
 
 cidadeComMaisVotosEmBranco = max(listaDeCidades,key=lambda x: x.votosEmBranco)
 print(f'**********************************************************************')
-print(f'A cidade com mais votos em Branco eh : {cidadeComMaisVotosNulos.nome} \n ')
-print(f'A quantidade total de votos eh : {cidadeComMaisVotosNulos.votosEmBranco} \n ')
-print(f'Que equivale a :   {cidadeComMaisVotosValidos.porcentagemDeVotosEmBranco:.2f}  % \n ')
+print(f'A cidade com mais votos em Branco eh : {cidadeComMaisVotosEmBranco.nome} \n ')
+print(f'A quantidade total de votos eh : {cidadeComMaisVotosEmBranco.votosEmBranco} \n ')
+print(f'Que equivale a :   {cidadeComMaisVotosEmBranco.porcentagemDeVotosEmBranco:.2f}  % \n ')
 print(f'\n**********************************************************************')
 
 
--- Exercicio5ConstrutoresEfuncoes.py
+++ Exercicio5ConstrutoresEfuncoes.py
@@ -1,4 +1,5 @@
 import random
+
 class Municipio:
     def __init__(self,nome:str, eleitores: list):
         self.eleitores = eleitores
@@ -27,3 +28,47 @@ def distribuirPopulacao(totalPopulacao, numSecoes):
     random.shuffle(listaPopulacao)
 
     return listaPopulacao
+
+
+def  entradaPeloUsuario():
+    listaInputdoUsuario = []
+    nomeDigitadoPeloUsuario = input("Digita o nome da cidade:")     
+        
+    votosEmBrancoUser = int(input("Quantos votos em branco na cidade: "))    
+    listaInputdoUsuario.append(votosEmBrancoUser)    
+
+    votosNulosUser = int(input("Quantos votos nulos na cidade: "))    
+    listaInputdoUsuario.append(votosNulosUser)    
+        
+    votosValidosUser = int(input("Quantos votos validos na cidade: "))    
+    listaInputdoUsuario.append(votosValidosUser)    
+    instanciaDeCidade = Municipio(nomeDigitadoPeloUsuario,listaInputdoUsuario)    
+    return instanciaDeCidade        
+
+def testeAutomatizado(x):
+    nomesCidadesReais = [
+    "São Paulo", "Rio de Janeiro", "Belo Horizonte", "Salvador", "Brasília", 
+    "Curitiba", "Fortaleza", "Recife", "Porto Alegre", "Manaus"]
+    x = 0
+    numeroAleatorio = random.randint(1,10000000000000)
+    populacaoAleatoria = distribuirPopulacao(numeroAleatorio,3)
+    instanciaDeCidade = Municipio(nomesCidadesReais[x],populacaoAleatoria)
+    return instanciaDeCidade
+
+
+
+def escolhaDeModo ():
+    escolhaFeita = False
+
+    while(not escolhaFeita):
+        escolha = int(input("Digie 1 para automatico"))
+        if(escolha == 1):
+            quantidadeDecidades = int(input("Digite a quantidade de cidades que vc deseja 1 - 10 "))
+            instanciaDeCidade = testeAutomatizado(quantidadeDecidades)
+            escolhaFeita = True
+            return instanciaDeCidade 
+        else:
+            instanciaDeCidade = entradaPeloUsuario ()
+            escolhaFeita = True
+            nstanciaDeCidade = entradaPeloUsuario()
+            return instanciaDeCidade 
